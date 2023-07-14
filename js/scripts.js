95318ccafbe02718ccea6b66a62344422066cde7
Darth Vader <danieltarancon@gmail.com>
2023-07-14 17:40:06 +0200

    14 1740

--- js/scripts.js
+++ js/scripts.js
@@ -22,30 +22,31 @@ xhr.onload = function() { // See main xhr methods
 
         let count = 0;
 
-        for(let i = 0; i < results.length; i++){
+        for(let i = 0; i < results.length; i++) {
 
-            if (count>=12){
+            if (count >= 12) {
 
                 break;
 
-            }}
+            }
 
-        let user = results[i];
+            let user = results[i];
 
-         let HTML = `<div class="card">
+            let HTML = `<div class="card">
          <div class="card-img-container">
          <img class="card-img" src=${user.picture.medium} alt="profile picture">
          </div>
          <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${user.name.first+' '+user.name.last}</h3>
+            <h3 id="name" class="card-name cap">${user.name.first + ' ' + user.name.last}</h3>
             <p class="card-text">${user.email}</p>
-            <p class="card-text cap">${user.location.city+', '+user.location.state}</p>
+            <p class="card-text cap">${user.location.city + ', ' + user.location.state}</p>
             
          </div>
          </div>`
 
             gallery.insertAdjacentHTML("beforeend", HTML);
             count++
+        }
 
         } else {
 
