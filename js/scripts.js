81a02e6ba827f277fe1198e583f8772a6b8d40ee
Darth Vader <danieltarancon@gmail.com>
2023-07-15 17:55:49 +0200

    15 1755

--- js/scripts.js
+++ js/scripts.js
@@ -193,3 +193,20 @@ function formatPhoneNumber(phoneNumber) {
   return formattedPhoneNumber
 
 }
+
+
+// Configurating the search tool
+
+
+const searchContainer = document.getElementsByClassName('search-container')[0];
+
+const HTMLSearchForm = `<form action="#" method="get">
+                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                        </form>`
+
+searchContainer.insertAdjacentHTML("beforeend", HTMLSearchForm);
+
+
+
+
